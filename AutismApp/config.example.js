2d63ebb96299b417ef0ca39c52cd0c9399f5b54e
GHOSTUSA <ethan.dahigermain@gmail.com>
2025-12-10 09:25:02 +0100

    remove useless files

--- AutismApp/config.example.js
+++
@@ -1,6 +0,0 @@
-// Configuration pour les clés API
-// Copiez ce fichier en config.js et mettez vos vraies clés
-export const CONFIG = {
-  CONVERT_API_TOKEN: "demo", // Remplacez par votre vraie clé ConvertAPI
-  GEMINI_API_KEY: "tamere", // Obtenez votre clé sur https://makersuite.google.com/app/apikey
-};
--- AutismApp/config.js
+++
@@ -1,5 +0,0 @@
-// Configuration pour les clés API
-export const CONFIG = {
-  CONVERT_API_TOKEN: "NI8bTXgw8zH1qBab78eyEKIjor5qEElT",
-  GEMINI_API_KEY: "tamere",
-};
